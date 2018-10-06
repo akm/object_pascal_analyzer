fbd6fe5fa14780d744ffb305867306f8269de348
akm <akm2000@gmail.com>
2018-10-06 16:50:43 +0900

    :green_heart: テストを使いまわしできるようにshared_examplesを定義しました
    テストの内容も若干変更。しかし、まだ実装してません。

--- spec/object_pascal_analyzer/loader_spec.rb
+++ spec/object_pascal_analyzer/loader_spec.rb
@@ -1,35 +1,65 @@
 require 'object_pascal_analyzer/loader'
 
 RSpec.describe ObjectPascalAnalyzer::Loader do
-  subject{ ObjectPascalAnalyzer::Loader.new }
-  it "loads directories" do
-    r = subject.load(File.expand_path("../../jedi-jvcl/tests/restructured/examples/HID/BasicDemo"), __FILE__)
-    expect(r).to be_an Array
-    expect(r.length).to eq 1
 
-    f0 = r.first
-    expect(f0.name).to eq 'Unit1.pas'
-    expect(f0.classes.length).to eq 1
+  # requires basic_demo_unit1
+  shared_examples_for 'basic_demo_unit1' do
+    let(:tform1){ basic_demo_unit1.classes['TForm1'] }
+    let(:hidctldevicechange){ tform1.methods['HidCtlDeviceChange'] }
+    let(:hidctlenumerate){ tform1.methods['HidCtlEnumerate'] }
 
-    c0 = f0.classes.first
-    expect(c0.path).to eq f0
-    expect(c0.name).to eq "TForm1"
-    expect(c0.methods.length).to eq 2
+    context "TForm1" do
+      subject{ tform1 }
+      it do
+        expect(subject.name).to eq "TForm1"
+        expect(subject.methods.length).to eq 2
+      end
+    end
 
-    m0 = c0.methods[0]
-    expect(m0.klass).to eq c0
-    expect(m0.name).to eq "HidCtlDeviceChange"
-    expect(m0.total_lines).to eq 2
-    expect(m0.empty_lines).to eq 0
-    expect(m0.comment_lines).to eq 0
-    expect(m0.max_depth).to eq 0
+    context "HidCtlDeviceChange" do
+      subject{ hidctldevicechange }
+      it do
+        expect(subject.klass).to eq tform1
+        expect(subject.name).to eq "HidCtlDeviceChange"
+        expect(subject.total_lines).to eq 2
+        expect(subject.empty_lines).to eq 0
+        expect(subject.comment_lines).to eq 0
+        expect(subject.max_depth).to eq 0
+      end
+    end
 
-    m1 = c0.methods[1]
-    expect(m1.klass).to eq c0
-    expect(m1.name).to eq "HidCtlEnumerate"
-    expect(m1.total_lines).to eq 4
-    expect(m1.empty_lines).to eq 0
-    expect(m1.comment_lines).to eq 0
-    expect(m1.max_depth).to eq 0
+    context "HidCtlEnumerate" do
+      subject{ hidctlenumerate }
+      it do
+        expect(subject.klass).to eq tform1
+        expect(subject.name).to eq "HidCtlEnumerate"
+        expect(subject.total_lines).to eq 4
+        expect(subject.empty_lines).to eq 0
+        expect(subject.comment_lines).to eq 0
+        expect(subject.max_depth).to eq 0
+      end
+    end
   end
+
+  let(:loader){ ObjectPascalAnalyzer::Loader.new }
+
+  File.expand_path("../../jedi-jvcl/tests/restructured/examples/HID/BasicDemo", __FILE__).tap do |path|
+    context path do
+      let(:result){ loader.load(path) }
+      let(:basic_demo_unit1){ result['Unit1.pas'] }
+
+      context "result" do
+        it { expect(result.length).to eq 1 }
+      end
+
+      context "unit1" do
+        subject{ basic_demo_unit1 }
+        it { expect(subject.name).to eq 'Unit1.pas' }
+        it { expect(subject.classes.length).to eq 1 }
+      end
+
+      it_behaves_like 'basic_demo_class'
+    end
+  end
+
 end
