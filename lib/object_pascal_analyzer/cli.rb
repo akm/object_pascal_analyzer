4d8159db88f3c3b2b481ee934b394c492ed61fcc
akm <akm2000@gmail.com>
2018-10-06 15:56:28 +0900

    :+1: 入出力の基本となるメソッドを定義しました

--- lib/object_pascal_analyzer/cli.rb
+++ lib/object_pascal_analyzer/cli.rb
@@ -4,6 +4,21 @@ require 'thor'
 
 module ObjectPascalAnalyzer
   class Cli < Thor
+    desc 'summary PATH_TO_DIR', 'Show summary'
+    def summary(path_to_dir)
+      $stdout.puts "Show summary of #{path_to_dir}"
+    end
+
+    desc 'csv PATH_TO_DIR', 'Show details in CSV'
+    def summary(path_to_dir)
+      $stdout.puts "Show details of #{path_to_dir} in CSV"
+    end
+
+    desc 'json PATH_TO_DIR', 'Show details in JSON'
+    def json(path_to_dir)
+      $stdout.puts "Show details of #{path_to_dir} in JSON"
+    end
+
     desc 'version', 'Show version'
     def version
       $stdout.puts ObjectPascalAnalyzer::VERSION
