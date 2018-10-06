b2215df634ac7ec27c8929fa6db96a89e8329e68
akm <akm2000@gmail.com>
2018-10-06 17:07:09 +0900

    :green_heart: 上位ディレクトリを指定した場合のロードテストを追加しました

--- spec/object_pascal_analyzer/loader_spec.rb
+++ spec/object_pascal_analyzer/loader_spec.rb
@@ -41,6 +41,81 @@ RSpec.describe ObjectPascalAnalyzer::Loader do
     end
   end
 
+  # requires thread_demo_mouse_reader
+  shared_examples_for 'thread_demo_mouse_reader' do
+    let(:tform1){ thread_demo_mouse_reader.classes['TForm1'] }
+    let(:hidctldevicechange){ tform1.methods['HidCtlDeviceChange'] }
+
+    let(:tmousethread){ thread_demo_mouse_reader.classes['TMouseThread'] }
+    let(:handlemousedata){ tmousethread.methods['HandleMouseData'] }
+    let(:execute        ){ tmousethread.methods['Execute'] }
+    let(:execute_dummy  ){ tmousethread.methods['Execute/Dummy'] }
+
+    context "TForm1" do
+      subject{ tform1 }
+      it do
+        expect(subject.name).to eq "TForm1"
+        expect(subject.methods.length).to eq 1
+      end
+    end
+
+    context "HidCtlDeviceChange" do
+      subject{ hidctldevicechange }
+      it do
+        expect(subject.klass).to eq tform1
+        expect(subject.name).to eq "HidCtlDeviceChange"
+        expect(subject.total_lines).to eq 38
+        expect(subject.empty_lines).to eq 0
+        expect(subject.comment_lines).to eq 7
+        expect(subject.max_depth).to eq 2
+      end
+    end
+
+    context "TMouseThread" do
+      subject{ tmousethread }
+      it do
+        expect(subject.name).to eq "TMouseThread"
+        expect(subject.methods.length).to eq 3
+      end
+    end
+
+    context "HandleMouseData" do
+      subject{ handlemousedata }
+      it do
+        expect(subject.klass).to eq tmousethread
+        expect(subject.name).to eq "HandleMouseData"
+        expect(subject.total_lines).to eq 6
+        expect(subject.empty_lines).to eq 0
+        expect(subject.comment_lines).to eq 2
+        expect(subject.max_depth).to eq 0
+      end
+    end
+
+    context "Execute" do
+      subject{ execute }
+      it do
+        expect(subject.klass).to eq tmousethread
+        expect(subject.name).to eq "Execute"
+        expect(subject.total_lines).to eq 18
+        expect(subject.empty_lines).to eq 0
+        expect(subject.comment_lines).to eq 5
+        expect(subject.max_depth).to eq 1
+      end
+    end
+
+    context "Execute/Dummy" do
+      subject{ execute_dummy }
+      it do
+        expect(subject.klass).to eq tmousethread
+        expect(subject.name).to eq "Execute/Dummy"
+        expect(subject.total_lines).to eq 0
+        expect(subject.empty_lines).to eq 0
+        expect(subject.comment_lines).to eq 0
+        expect(subject.max_depth).to eq 0
+      end
+    end
+  end
+
   let(:loader){ ObjectPascalAnalyzer::Loader.new }
 
   File.expand_path("../../jedi-jvcl/tests/restructured/examples/HID/BasicDemo", __FILE__).tap do |path|
@@ -58,7 +133,28 @@ RSpec.describe ObjectPascalAnalyzer::Loader do
         it { expect(subject.classes.length).to eq 1 }
       end
 
+      it_behaves_like 'basic_demo_unit1'
+    end
+  end
+
+  File.expand_path("../../jedi-jvcl/tests/restructured/examples/HID", __FILE__).tap do |path|
+    context path do
+      let(:result){ loader.load(path) }
+      let(:basic_demo_unit1){ result['BasicDemo/Unit1.pas'] }
+      let(:thread_demo_mouse_reader){ result['ThreadDemo/MouseReader.pas'] }
+
+      context "result" do
+        it { expect(result.length).to eq 5 }
+      end
+
+      context "unit1" do
+        subject{ unit1 }
+        it { expect(subject.name).to eq 'BasicDemo/Unit1.pas' }
+        it { expect(subject.classes.length).to eq 1 }
+      end
+
       it_behaves_like 'basic_demo_class'
+      it_behaves_like 'thread_demo_mouse_reader'
     end
   end
 
