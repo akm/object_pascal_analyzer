95422994ca59f08fff04ed45c7d6fdb9c3b414df
akm <akm2000@gmail.com>
2018-10-13 11:36:56 +0900

    :green_heart: テストを追加する前に共通で使えそうなsubjectをdescribeの外に移動しました

--- spec/object_pascal_analyzer/to_hash_spec.rb
+++ spec/object_pascal_analyzer/to_hash_spec.rb
@@ -1,34 +1,34 @@
 require 'object_pascal_analyzer/pascal_file'
 
 RSpec.describe ObjectPascalAnalyzer::PascalFile do
-  describe :to_hash do
-    subject do
-      ObjectPascalAnalyzer::PascalFile.new("unit1.pas").tap do |pf|
-        pf.class_by("TForm1").tap do |klass|
-          klass.function_by("Button1Click").tap do |f|
-            f.total_lines = 10
-            f.empty_lines = 2
-            f.comment_lines = 3
-            f.max_depth = 1
-          end
-          klass.function_by("Input1Change").tap do |f|
-            f.total_lines = 40
-            f.empty_lines = 2
-            f.comment_lines = 10
-            f.max_depth = 3
-          end
+  subject do
+    ObjectPascalAnalyzer::PascalFile.new("unit1.pas").tap do |pf|
+      pf.class_by("TForm1").tap do |klass|
+        klass.function_by("Button1Click").tap do |f|
+          f.total_lines = 10
+          f.empty_lines = 2
+          f.comment_lines = 3
+          f.max_depth = 1
         end
-        pf.class_by("THelper").tap do |klass|
-          klass.function_by("run").tap do |f|
-            f.total_lines = 20
-            f.empty_lines = 3
-            f.comment_lines = 0
-            f.max_depth = 2
-          end
+        klass.function_by("Input1Change").tap do |f|
+          f.total_lines = 40
+          f.empty_lines = 2
+          f.comment_lines = 10
+          f.max_depth = 3
+        end
+      end
+      pf.class_by("THelper").tap do |klass|
+        klass.function_by("run").tap do |f|
+          f.total_lines = 20
+          f.empty_lines = 3
+          f.comment_lines = 0
+          f.max_depth = 2
         end
       end
     end
+  end
 
+  describe :to_hash do
     it do
       expected = {
         path: "unit1.pas",
