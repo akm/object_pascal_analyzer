1495a9f45101a3202a21d58c10d1ff27d09a6a0e
akm <akm2000@gmail.com>
2018-10-19 07:58:47 +0900

    :+1: テーブルを表示する機能を追加しました

--- lib/object_pascal_analyzer/cli.rb
+++ lib/object_pascal_analyzer/cli.rb
@@ -8,7 +8,13 @@ module ObjectPascalAnalyzer
   class Cli < Thor
     desc 'summary PATH_TO_DIR', 'Show summary'
     def summary(path_to_dir)
-      output "Show summary of #{path_to_dir}"
+      pascal_files = ObjectPascalAnalyzer.load(path_to_dir)
+      functions = pascal_files.map(&:functions).flatten.map{|f| f.to_hash(full: true)}
+
+      result = [
+        build_table(functions),
+      ]
+      output result.join("\n")
     end
 
     CSV_HEADERS = [
@@ -51,6 +57,56 @@ module ObjectPascalAnalyzer
     no_commands do
       def output(value)
         $stdout.puts(value)
+      end
+
+      ALIGNMENTS = {
+        path: '-',
+        class: '-',
+        name: '-',
+        total_lines: '',
+        empty_lines: '',
+        comment_lines: '',
+        max_depth: '',
+      }
+
+      FORMAT_TYPES = {
+        path: 's',
+        class: 's',
+        name: 's',
+        total_lines: 'd',
+        empty_lines: 'd',
+        comment_lines: 'd',
+        max_depth: 'd',
+      }
+
+      TITLES = {
+        path: 'Path',
+        class: 'Class',
+        name: 'Name',
+        total_lines: 'Total',
+        empty_lines: 'Empty',
+        comment_lines: 'Comment',
+        max_depth: 'Depth',
+      }
+
+      def build_table(functions)
+        max_lengths = CSV_HEADERS.each_with_object({}) do |col, d|
+          d[col] = (functions.map{|f| f[col]}.map(&:to_s) + [TITLES[col]]).map(&:length).max
+        end
+        row_format = CSV_HEADERS.map{|col| "%#{ALIGNMENTS[col]}#{max_lengths[col]}\{#{col.to_s}\}" }.join(' ')
+        header_format = CSV_HEADERS.map{|col| "%-#{max_lengths[col]}\{#{col.to_s}\}" }.join(' ')
+
+        puts "header_format"
+        puts header_format.inspect
+        puts "row_format"
+        puts row_format.inspect
+        result = [header_format % TITLES]
+
+        functions.each do |f|
+          p f
+          result << (row_format % f)
+        end
+        result.join("\n")
       end
     end
   end
